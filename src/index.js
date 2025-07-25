6e18ebd9b820a637ed50a81eb40ca84b9ab1cef7
reenahariproject <harivip147@gmail.com>
2025-07-25 14:54:30 +0530

    Working changes before switching to gh-pages

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import SplashCursor from './components/SplashCursor';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      
+      <BrowserRouter basename="/funny"/>
     <SplashCursor />
     <App />
   </React.StrictMode>
