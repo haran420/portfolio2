583d86667f77a2af213cd871b7aec5f7ac40b1a7
reenahariproject <harivip147@gmail.com>
2025-11-12 17:15:13 +0530

    commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Forntend Developer", "Java Developer", "Software Developer" ];
+  const toRotate = [ "Forntend Developer", "Web Developer", "Software Developer" ];
   const period = 2000;
 
   useEffect(() => {
